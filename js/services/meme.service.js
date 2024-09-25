b3230edd7c09fc8236cb7d4e4252900c3f64d21f
matan-ode <matanodentz@gmail.com>
2024-09-25 12:11:47 +0300

    Init project base

---
+++ js/services/meme.service.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// var gImgs
+
+// var gMemes
+// var gMeme
+
+// var gKeywordSearchCountMap 
+
+
+// function createImgs(){
+//     if(gMemes && gMemes.length !== 0) return
+
+//     createImg(1, keywords)
+// }
+
+// function createImg(id, keywords){
+//     return {
+//         id,
+//         url: `/img/${id}.jpg`,
+//         keywords
+//     }
+// }
