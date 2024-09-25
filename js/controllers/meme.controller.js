ca7cbe0858bfdbf66d49c6076d9abddbb4ef9bad
matan-ode <matanodentz@gmail.com>
2024-09-25 13:01:04 +0300

    Make basic canvas on html

--- js/controllers/meme.controller.js
+++ js/controllers/meme.controller.js
@@ -1,1 +1,38 @@
 'use strict'
+
+// let gElCanvas
+// let gCtx
+
+function onInit() {
+    // gElCanvas = document.querySelector('canvas')
+    // gCtx = gElCanvas.getContext('2d')
+    // addListeners()
+    // resizeCanvas()
+    createImgs()
+}
+
+function renderMeme(imgId) {
+    
+    createMeme(imgId)
+    getMeme()
+}
+
+// function renderCanvas() {
+//     gCtx.fillStyle = '#ffffff'
+//     gCtx.fillRect(0, 0, gElCanvas.width, gElCanvas.height)
+// }
+
+// function resizeCanvas() {
+//     const elContainer = document.querySelector('.canvas-container')
+//     gElCanvas.width = elContainer.offsetWidth
+//     gElCanvas.height = elContainer.offsetHeight
+//     renderCanvas()
+
+// }
+
+// function addListeners() {
+//     window.addEventListener('resize', () => {
+//         resizeCanvas()
+//         renderCanvas()
+//     })
+// }
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -1,23 +1,59 @@
 'use strict'
 
-// var gImgs
+var gImgs
 
 // var gMemes
-// var gMeme
+var gMeme
 
-// var gKeywordSearchCountMap 
+var gKeywordSearchCountMap
 
 
-// function createImgs(){
-//     if(gMemes && gMemes.length !== 0) return
 
-//     createImg(1, keywords)
-// }
+function createMeme(imgId) {
+    gMeme = {
+        selectedImgId: imgId,
+        selectedLineIdx: 0,
+        lines: [
+            {
+                txt: 'I sometimes eat Falafel',
+                size: 20,
+                color: 'red'
+            }
+        ]
+    }
+}
 
-// function createImg(id, keywords){
-//     return {
-//         id,
-//         url: `/img/${id}.jpg`,
-//         keywords
-//     }
-// }
+function getMeme() {
+    return gMeme
+}
+
+function createImgs() {
+    gImgs = []
+    // if(gMemes && gMemes.length !== 0) return
+    gImgs.push(createImg(1, ['funny', 'politics']))
+    gImgs.push(createImg(2, ['dog']))
+    gImgs.push(createImg(3, ['dog', 'baby']))
+    gImgs.push(createImg(4, ['cat']))
+    gImgs.push(createImg(5, ['funny', 'baby']))
+    gImgs.push(createImg(6, ['crazy']))
+    gImgs.push(createImg(7, ['funny', 'baby']))
+    gImgs.push(createImg(8, ['crazy']))
+    gImgs.push(createImg(9, ['funny', 'baby']))
+    gImgs.push(createImg(10, ['funny', 'politics']))
+    gImgs.push(createImg(11, ['crazy', 'sport']))
+    gImgs.push(createImg(12, ['israeli', 'funny']))
+    gImgs.push(createImg(13, ['movie', 'funny']))
+    gImgs.push(createImg(14, ['movie', 'serious']))
+    gImgs.push(createImg(15, ['movie', 'serious']))
+    gImgs.push(createImg(16, ['movie', 'funny']))
+    gImgs.push(createImg(17, ['funny', 'politics']))
+    gImgs.push(createImg(18, ['movie', 'funny']))
+}
+
+function createImg(id, keywords) {
+    return {
+        id,
+        url: `/img/${id}.jpg`,
+        keywords
+    }
+}
